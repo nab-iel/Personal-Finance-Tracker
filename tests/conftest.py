d5426013c83568a963576ffb5eb363f31c60b902
Nabiel <78254324+nab-iel@users.noreply.github.com>
2025-09-03 22:34:54 +0100

    Added conftest

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from sqlalchemy.orm import Session
+
+from app.database import Base, engine
+from models import User
+
+@pytest.fixture(scope="function")
+def session():
+    if not engine.url.get_backend_name() == "sqlite":
+        raise RuntimeError('Use SQLite backend to run tests')
+
+    Base.metadata.create_all(engine)
+    try:
+        with Session() as session:
+            yield session
+    finally:
+        Base.metadata.drop_all(engine)
+
+@pytest.fixture(scope="function")
+def seed(session):
+    session.add_all(
+        [
+            User(id=1, username="example1", email="example1@gmail.com", password="123"),
+            User(id=2, username="example2", email="example2@gmail.com", password="456"),
+        ]
+    )
+    session.commit()
